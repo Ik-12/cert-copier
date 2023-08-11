f8e9e49c0e2bec5b382cd77e409c9b970f8c0e04
Ik-12 <78494704+Ik-12@users.noreply.github.com>
2023-08-11 19:27:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ CERTPATH=/certs/live
 
 while [ true ]
 do
-	fswatch -d $CERTPATH
-	echo "File change detected."
+  fswatch -d $CERTPATH
+  echo "File change detected."
 
   for d in $CERTPATH/*/
     do
-      echo "Processing host $d..."
+      echo "Updating host $d..."
       cp $d\privkey.pem $d.key
       cp $d\fullchain.pem $d.crt
    done
